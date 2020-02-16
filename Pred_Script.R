fee9333b6b5d8747ee9c746c0cd4f3458fa3d3b0
bobshriver <bshriver59@gmail.com>
2020-02-16 14:26:57 -0800

    Finalize 1st draft of pred script

--- Pred_Script.R
+++ Pred_Script.R
@@ -1,4 +1,5 @@
 ###Step 1### 
+library(rstan)
 
 # Set working directory to source file location 
 # (this method only works on Rstudio)
@@ -7,12 +8,23 @@
 #setwd(dirname(current_path )) # set working directory to location of this file
 
 ##Load workspace with Stan model fit.##
-load("./../data/future/by_scenario/covariates_sc")
+load("./../parameters/........")
 
 # MAKE FILENAMES
 scenars <- c(1,seq(25,45,2)) # 1 is current, the others are all the RCP85, late century scenarios
 
-ecoregion <- "california" # for example for 
+ecoregion <- "CA" #name of ecoregion: CA, SGS, CD, NM, HD
+
+###These lines make sure the model fit data match what the region specified is
+if (ecoregion=="CA"){HistData=CA;fit=CAfit}
+if (ecoregion=="CD"){HistData=CD;fit=CDfit}
+if (ecoregion=="SGS"){HistData=SGS;fit=SGSfit}
+if (ecoregion=="NM"){HistData=NM;fit=NMfit}
+if (ecoregion=="HD"){HistData=HD;fit=HDfit}
+
+###Read in sitematch table 
+matchtable<-read.csv("./../data/site_coordinate_match.csv")
+HistData<-merge(HistData,matchtable)
 
 for(i in scenars){
   
@@ -30,12 +42,31 @@ NySim<-length(FYrs)#Number of simulation years in each scenario.
 ###########################
 ###Add command to subset covariates by region and match sites####
 ###########################
-
+###Remove sites not in ecoregion
 ####need vector to match sites in X with random effects
-SiteMatch<-
+unqsite<-HistData$site[which(HistData$year==1986)] ###Take the name of each site by extracting 1 year of data. 
+SiteMatch<-match(as.character(future_covars$site),unqsite) ###Match site names of sites with historic data locations in the historic data
+
+future_covars<-future_covars[which(is.na(SiteMatch)==F),] ###remove locations that do not match with region sites
+future_covars<-merge(future_covars,matchtable) ###as a backup merge x and y with future data. 
+
+SiteMatch<-SiteMatch[which(is.na(SiteMatch)==F)]####This is the index needed for the site random effect below #remove NAs
 
 ###need to find mean for each site, and create annual deviates for each pixel
-XfutUns<-
+
+sitemeanPHist<-HistData$mm.mean[which(HistData$year==1986)] ###Get historic site mean precip and temp by just extracting one year
+sitemeanTHist<-HistData$WatYrTEMP.mean[which(HistData$year==1986)]
+
+sitemeanPFut<-aggregate((future_covars[,3]*10)~future_covars[,1],FUN=mean)[,2]
+sitemeanTFut<-aggregate(future_covars[,4]~future_covars[,1],FUN=mean)[,2]
+
+future_covars$mm.mean<-rep(sitemeanPFut,each=NySim)
+future_covars$mm.dev<-(future_covars$WatYrPRECIP*10)/sitemeanP[SiteMatch] ###get future deviations from the historic mean ###convert future values from cm to mm
+future_covars$WatYrTEMP.mean<-rep(sitemeanTFut,each=NySim)
+future_covars$WatYrTEMP.dev<-(future_covars$WatYrTEMP)/sitemeanT[SiteMatch] ###get future deviations from the historic mean
+
+
+Xfutuns<-future_covars[,c('mm.mean','mm.dev','WatYrTEMP.mean','WatYrTEMP.dev')]###Extract future covs in a way that matches historic data
   
   
 Xfut<-t((t(Xfutuns)-apply(Xuns,2,mean))/apply(Xuns,2,sd))
@@ -88,7 +119,7 @@ for (p in 1:Iter) { ##This is a loop over all of the different parameter sets fr
 
 ###Add colnames to file for site and year ####
 Pred.out<-rbind(FYrs,Pred.out)###append the year to the first row 
-rownames(Pred.out)<-rep() ####rep site names for 
+rownames(Pred.out)<-future_covars$site ####rep site names for 
 
 
 # write output to file...though maybe it will be .rds (arrays) instead of .csv?
